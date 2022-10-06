2e19ff0681e017e2d28bd21d506d2d324152040b
Sohel Mukadam <34812803+sohelmukadam@users.noreply.github.com>
2022-10-06 17:44:23 +0530

    Update NewTest.java

--- src/test/NewTest.java
+++ src/test/NewTest.java
@@ -22,6 +22,7 @@ public class NewTest {
 		driver.findElement(By.id("login")).click();
 		String actualTitle = driver.getTitle();
 		Assert.assertEquals("Adactin.com - Search Hotel", actualTitle);
+	  	System.out.println("New line");
   }
   @BeforeTest
   public void beforeTest() {
